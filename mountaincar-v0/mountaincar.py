93790f75b970584d8337a87ec17f4083581bfa33
Nicholas Vadivelu <nick@bassoft.com>
2017-09-03 00:13:18 -0400

    started mountaincar

--- mountaincar-v0/mountaincar.py
+++ mountaincar-v0/mountaincar.py
@@ -2,7 +2,7 @@
 #   Created On: 2 September 2017
 #   Last Updated: 2 September 2017
 
-#   Train Neural Network with Open AI
+#   Solve the MountainCar environment 
 
 import gym #open ai gym
 import random #initially random moves
@@ -11,41 +11,42 @@ import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 from statistics import mean, median 
-from collections import Counter
 
 LR = 1e-3 #learning rate
-env = gym.make('CartPole-v0')
+env = gym.make('MountainCar-v0')
 env.reset()
-goal_steps = 500 #how many frames it can stay up
-score_requirement = 50 #learn from games that get a score of this or above
+score_requirement = -150 #learn from games that get a score of this or above
+pos_requirement = -0.2
 initial_games = 5000
 n_games = 20
-keep_rate = 0.8
-num_actions = 2
+num_actions = 3
 
 def some_random_games_first():
     for episode in range(5):
         env.reset()
-        for t in range(goal_steps):
+        for t in range(100):
             env.render() #to see what's going on, will go faster if you disable
             action = env.action_space.sample() #takes a random action in your environment
             #observation is info from game (lot of time can be pixel data, for this game it's physics-based data)
             #reward is 1 or 0, so balanced or not
             #done is whether game is done or not
             observation, reward, done, info = env.step(action)
+            print(observation)
             if done:
                 break
 
 def reinforced_learning(model, n_games=10):
-    training_data = []
-    scores = []
-    accepted_scores = [0]
+    accepted_scores = [-200]
+    prev_max_pos = [-1.2]
     game_num = 1
     total_games = initial_games
+
     while game_num <= n_games:
         score = 0
         game_memory = [] #store movements
         prev_obs = []
+        training_data = []
+        max_pos = -1.2
         done = False
         while not done:
             if len(prev_obs) == 0:
@@ -54,33 +55,38 @@ def reinforced_learning(model, n_games=10):
                 action = np.argmax(model.predict(prev_obs.reshape(-1,len(prev_obs), 1))[0])
             
             observation, reward, done, info = env.step(action)
+            
+            if observation[0] > max_pos:
+                max_pos = observation[0]
 
             if len(prev_obs) > 0:
                 game_memory.append([prev_obs, action]) #because this action caused current observation, so previous observation was what lead to current action
             
             prev_obs = observation
-            score += reward #reward is one or zero (zero if lost)
+            score = score - 1
 
-            if score > 1000:
-                break
-
-        if score >= accepted_scores[-1]: #it's better than the last game
+        if score > accepted_scores[-1] or max_pos >= prev_max_pos[-1]: #it's better than the last game
+            prev_max_pos.append(max_pos)
             accepted_scores.append(score)
             game_num = game_num + 1
             for data in game_memory: 
-                output = np.zeros(num_actions);
+                output = [0]*num_actions
                 output[data[1]] = 1
                 
                 training_data.append([data[0], output])
+                
             model = train_model(training_data, model)
-            print('Game Number:', game_num, '. Last Score:', accepted_scores[-1], '. Total Games:', total_games)
+            print('Game Number:', game_num, '. Last Score:', accepted_scores[-1], 'Last Max Pos:', prev_max_pos[-1], '. Total Games:', total_games)
+        
         total_games = total_games + 1
-        training_data = []
+        
+        if total_games%50 == 0:
+            print('Game Number:', game_num, '. Last Score:', accepted_scores[-1], 'Last Max Pos:', prev_max_pos[-1], '. Total Games:', total_games)
+
         env.reset()
         
     print('Average accepted score:', mean(accepted_scores))
     print('Median accepted score:', median(accepted_scores))
-    print(Counter(accepted_scores)) #counts how many of each score appear
 
     return model
 
@@ -88,11 +94,14 @@ def initial_population():
     training_data = [] #observation and move made (all random), but only use if score above 50
     scores = []
     accepted_scores = []
+    done = False
     for _ in range(initial_games): #underscore says variables doesn't matter, just tryna iterate
         score = 0
         game_memory = [] #store movements
         prev_observation = []
-        for _ in range (goal_steps):
+        max_pos = -1.2
+        done = False
+        while not done:
             action = env.action_space.sample()
             #()random.randrange(0, 2) #env.action_space.sample() might be better, but this corresponds to left and right movement
             observation, reward, done, info = env.step(action)
@@ -100,50 +109,40 @@ def initial_population():
             if len(prev_observation) > 0:
                 game_memory.append([prev_observation, action]) #because this action caused current observation, so previous observation was what lead to current action
             
-            prev_observation = observation
-            score += reward #reward is one or zero (zero if lost)
+            if observation[0] > max_pos:
+                max_pos = observation[0]
 
-            if done:
-                break
+            prev_observation = observation
             
-        if score >= score_requirement: #if it's a winning game
+            score = score - 1
+
+        if score >= score_requirement or max_pos > pos_requirement: #if it's a winning game
+            print(score, max_pos)
             accepted_scores.append(score)
 
             for data in game_memory: 
-                if data[1] == 1:
-                    output = [0,1] 
-                elif data[1] == 0:
-                    output = [1,0]
-                
+                output = [0]*num_actions
+                output[data[1]] = 1
+
                 training_data.append([data[0], output])
         
         env.reset()
-        scores.append(scores)
     
     print('Average accepted score:', mean(accepted_scores))
     print('Median accepted score:', median(accepted_scores))
-    print(Counter(accepted_scores)) #counts how many of each score appear
+    print('Number of examples: ', len(accepted_scores))
     return training_data
 
 def neural_network_model(input_size):
     network = input_data(shape=[None, input_size, 1], name='input')
 
     network = fully_connected(network, 128, activation = 'relu') #input layer
-    network = dropout(network, keep_rate) #0.8 is keep rate
 
     network = fully_connected(network, 256, activation = 'relu') #input layer
-    network = dropout(network, keep_rate) #0.8 is keep rate
-
-    network = fully_connected(network, 512, activation = 'relu') #input layer
-    network = dropout(network, keep_rate) #0.8 is keep rate
-
-    network = fully_connected(network, 256, activation = 'relu') #input layer
-    network = dropout(network, keep_rate) #0.8 is keep rate
 
     network = fully_connected(network, 128, activation = 'relu') #input layer
-    network = dropout(network, keep_rate) #0.8 is keep rate
 
-    network = fully_connected(network, 2, activation='softmax')
+    network = fully_connected(network, num_actions, activation='softmax')
     network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
 
     model = tflearn.DNN(network, tensorboard_verbose=0)
@@ -151,7 +150,7 @@ def neural_network_model(input_size):
     return model
 
 def train_model(training_data, model=False, n_epochs=1):
-    print(training_data)
+    #print([i[0] for i in training_data])
     X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]), 1) #gets observations
     y = [i[1] for i in training_data]
 
@@ -167,11 +166,12 @@ def train_model(training_data, model=False, n_epochs=1):
 #some_random_games_first() #see if things are working as you expect
 
 training_data = initial_population()
+print('Going to train')
 model = train_model(training_data, n_epochs=3)
+print('Done training')
 model = reinforced_learning(model, n_games)
 model.save('cartpole_model-improved.model') #could save the  model and import for longer ones
 #model.load('cartpole_model.model')
-
 scores = []
 choices = []
 
@@ -180,8 +180,8 @@ for each_game in range(100): #very similar to above function, could probably com
     game_memory = []
     prev_obs = []
     env.reset()
-
-    for _ in range(goal_steps):
+    done = False
+    while not done:
         #env.render()
         if len(prev_obs) == 0:
             action = random.randrange(0, 2)
